7a07112e7a735f2db0e065c690d264ae523dc5b3
Jason Oyato <jasonoyato@yahoo.com>
2022-12-09 00:55:47 +0300

    working on c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,8 @@
+#include <stdio.sh>
+
+int main(void)
+{
+	puts(" with proper grammar, but the outcome is a piece of art, ");
+	return (o);
+}
+
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+	puts("print types c is compiled and run on in computer");
+	return (0);
+}
+
